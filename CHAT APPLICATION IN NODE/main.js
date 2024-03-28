a7726470ecb26e04528d74377fda16a6475f795c
Tamzid-Ahammad <157707820+Tamzid-Ahammad@users.noreply.github.com>
2024-03-28 13:51:33 +0600

    Add files via upload
    Making Chat Application using HTML,JS ,SOCKET.IO

---
+++ CHAT APPLICATION IN NODE/main.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var http = require('http');
+var socketIO = require('socket.io');
+var app = express();
+var server = http.createServer(app);
+
+var io = socketIO(server);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (rq, res) => {
+    res.sendFile(__dirname + "/public/chat.html");
+})
+
+io.on('connection', function (socket) {
+
+    socket.on('newuser', function (user) {
+        socket.username = user;
+
+        socket.emit('connection.success', ResponseMessage("Server", "hello " + socket.username + ", successfully connected"));
+
+        socket.broadcast.emit('connection.others', ResponseMessage("Server", socket.username + " has connected"));
+
+    })
+
+    socket.on('send', function (msg) {
+
+        io.emit("SendClient", ResponseMessage(socket.username, msg));
+    });
+   
+    socket.on('uploadImage', function (msg) {
+
+        io.emit("SendImage", ResponseMessage(socket.username, msg));
+    });
+
+
+    socket.on('disconnect', function () {
+
+        socket.broadcast.emit("connection.others", ResponseMessage("Server", socket.username + " has left!"));
+    });
+
+});
+
+
+function ResponseMessage(u, msg) {
+    return { UserName: u, Message: msg, Time: new Date().toTimeString() };
+}
+
+server.listen(80);
+console.log('app started at : 80');
